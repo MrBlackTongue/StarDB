ef4487de789dd31583bd34e20b24835fcb28fffa
Svetlana Tyshkevich <Lana-14-92@mail.ru>
2021-02-04 19:19:15 +0300

    change app

--- src/components/row/row.js
+++ src/components/row/row.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import './row.css'
 
-const Row = ({left, right}) => {
+const Row = ({ left, right }) => {
     return (
         <div className='row mb2'>
             <div className='col-md-6'>
